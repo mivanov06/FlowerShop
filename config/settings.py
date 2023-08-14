5b2ee969e0ace02206f9aa4d7f15aa7c58c52a5a
Stranix <Phantom2525@gmail.com>
2023-08-14 16:47:41 +0300

    Добавил новую настройку TG_BOT_TOKEN для телеграм бота.

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
+TG_BOT_TOKEN = env.str('TG_BOT_TOKEN', '')
 
 # Application definition
 
