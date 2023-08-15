6820af71a5ae702a0f52f17b7248addb28ca7828
Stranix <Phantom2525@gmail.com>
2023-08-15 11:26:54 +0300

    Задал маршруты предполагаемых страниц сайта.

--- src/urls.py
+++ src/urls.py
@@ -5,4 +5,9 @@ from src import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('/catalog', views.catalog, name='catalog'),
+    path('/recommendations', views.recommendations, name='recommendations'),
+    path('/contacts', views.contacts, name='contacts'),
+    path('/selection_flowers', views.selection_flowers, name='selection_flowers'),
+    path('/orders', views.orders, name='orders'),
 ]
--- src/views.py
+++ src/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, template_name='index.html')
+
+
+def catalog(request):
+    pass
+
+
+def recommendations(request):
+    pass
+
+
+def contacts(request):
+    pass
+
+
+def selection_flowers(request):
+    pass
+
+
+def orders(request):
+    pass
