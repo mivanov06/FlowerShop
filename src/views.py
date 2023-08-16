2ad01b3a6250082697b255cb83e965f76b5efb1e
Stranix <Phantom2525@gmail.com>
2023-08-16 12:52:47 +0300

    Добавил логику для страницы Рекомендации.

--- src/views.py
+++ src/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from src.models import Event, Bouquet
+
 
 def index(request):
     return render(request, template_name='pages/index.html')
@@ -10,7 +12,36 @@ def catalog(request):
 
 
 def recommendations(request):
-    return render(request, template_name='pages/recommendations.html')
+    step = 1
+    events = Event.objects.all()
+
+    event = request.session.get('event')
+    price = request.session.get('price')
+    bouquet = None
+
+    if request.method == 'GET':
+        request.session.pop('event', '')
+        request.session.pop('price', '')
+
+    if request.method == 'POST' and not event:
+        request.session['event'] = request.POST.get('event')
+        step = 2
+
+    if request.method == 'POST' and event and not price:
+        request.session['price'] = request.POST.get('price')
+        price = request.POST.get('price').split('-')
+        bouquet = Bouquet.objects.filter(price__range=tuple(price)).first()
+        step = 3
+
+    return render(
+        request,
+        template_name='pages/recommendations.html',
+        context={
+            'events': events,
+            'step': step,
+            'bouquet': bouquet,
+        }
+    )
 
 
 def contacts(request):
@@ -21,5 +52,5 @@ def bouquet_card(request, pk):
     return render(request, template_name='pages/card.html')
 
 
-def order(request):
+def order(request, pk):
     return render(request, template_name='pages/order.html')
